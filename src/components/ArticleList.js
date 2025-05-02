9854d96f98e2164410b788124624cc46bf582d97
MarkMusonye <markmusonye@gmail.com>
2025-05-02 08:43:00 +0300

    Initialized my codebase

---
+++ src/components/ArticleList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({ posts }) {
+  return (
+    <main>
+      {posts.map(({ id, title, date, preview }) => (
+        <Article key={id} title={title} date={date} preview={preview} />
+      ))}
+    </main>
+  );
+}
+
+export default ArticleList;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+header.js;
+import React from "react";
+
+function Header({ name }) {
+  return (
+    <Header>
+      <h1>{name}</h1>
+    </Header>
+  );
+}
+export default Header;
